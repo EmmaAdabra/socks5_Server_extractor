74eb7a426ff63f4f610ce10476b6c56cf7477abd
EmmaAdabra <adabraemma@gmail.com>
2024-09-29 20:12:51 -0400

    Add a main function, that will contain the logic and flow of the script

--- script.py
+++ script.py
@@ -15,3 +15,20 @@ def get_socks_images(image_dir):
   ]
 
   return socks_images
+
+def main():
+  """controls program flow and logic"""
+  # check if source directory exist
+  if not os.path.exists(socks_images_dir):
+     print("Directory does not exist")
+     sys.exit(1)
+  # check if source directory is empty
+  if len(os.listdir(socks_images_dir)) == 0:
+    print("Directory is empty")
+    sys.exit(1)
+  socks_images = get_socks_images(socks_images_dir)
+  print(len(socks_images))
+
+
+if __name__ == "__main__":
+  main()
