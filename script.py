a177239f9d8d2c6d3627baca95f12041d05772b8
EmmaAdabra <adabraemma@gmail.com>
2024-10-02 04:58:57 -0400

    Modify add_ip_port_to_list function
    This function was modify to use subprocesses to extract text from
    images

--- script.py
+++ script.py
@@ -5,13 +5,14 @@ import sys
 import pytesseract
 import re
 import csv
+from multiprocessing import Pool
 
 # Set the path to tesseract.exe
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # the dir that contains the images should be in the same directory with script file and should be name images
 current_dir = os.path.dirname(os.path.abspath(__file__))
-socks_images_dir = os.path.join(current_dir, "socks_images")
+socks_images_dir = os.path.join(current_dir, "images")
 csv_dir = os.path.join(current_dir, "csv")
 data = []
 
@@ -26,10 +27,11 @@ def get_socks_images(image_dir):
 
 
 def extract_text_from_image(image_path):
-  image = Image.open(image_path)
-
-  return pytesseract.image_to_string(image)
-
+  try:
+    image = Image.open(image_path)
+    return pytesseract.image_to_string(image)
+  except Exception as e:
+    return None
 
 def extract_ips_and_ports(text):
   # Updated regex pattern to capture IPs with optional spaces
@@ -50,9 +52,11 @@ def extract_ips_and_ports(text):
 
 def add_ip_port_to_list(socks_images):
   try:
-    for image in socks_images:
-      image_text_content = extract_text_from_image(image)
-      ip_and_port = extract_ips_and_ports(image_text_content)
+    # using subprocesses
+    with Pool(processes=4) as pool:
+      extracted_texts = pool.map(extract_text_from_image, socks_images)
+    for text in extracted_texts:
+      ip_and_port = extract_ips_and_ports(text)
       data.extend(ip_and_port)
   except Exception as e:
     print(f"An error occurred: {e}")
@@ -67,6 +71,9 @@ def save_to_csv(data):
     writer.writeheader()  # Write the header
     writer.writerows(data)  # Write the data rows
 
+  print("Success !!!")
+  print(f"{len(data)} servers extracted from {len(get_socks_images(socks_images_dir))} images and save to {csv_dir}")
+
 
 def main():
   """controls program flow and logic"""
@@ -77,13 +84,12 @@ def main():
 
   # check if source directory is empty
   if len(os.listdir(socks_images_dir)) == 0:
-    print("Directory is empty")
+    print("Image directory is empty")
     sys.exit(1)
 
   socks_images = get_socks_images(socks_images_dir)
   add_ip_port_to_list(socks_images)
   save_to_csv(data)
-  # print(data)
 
 if __name__ == "__main__":
   main()
